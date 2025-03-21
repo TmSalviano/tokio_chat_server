15905a831be2f754c1a948606e9e5bb02ae31daa
TmSalviano <tiagomsalviano@outlook.com>
2025-03-21 03:43:23 -0300

    Success: single client to server data transfer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
 pub mod server;
 
+use core::panic;
+use std::io::Read;
+
 use server::Server;
 //matches many different Results for convenience
 use anyhow::Result;
 //logs at the info level
 use log::info;
-use tokio::net::TcpListener;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let ip_addr = "127.0.0.1:8080";
+    let server = TcpListener::bind(ip_addr).await?;
+    println!("Passive open was established");
 
-    let listener = match TcpListener::bind(ip_addr).await {
-        Ok(the_listener) => {
-            info!("TCP listener started on port 8080");
-            the_listener
-        }
-        Err(e) => panic!("Failed to bind to localhost:8080"),
+    let socket = match server.accept().await {
+        Ok((socket, _)) => socket,
+        Err(_) => panic!("Failed to establish an active connection"),
     };
 
-    let server = Server::new(listener);
+    handle_socket(socket).await?;
+
     Ok(())
+}
+
+async fn handle_socket(mut socket: TcpStream) -> Result<()> {
+    //1kb capacity for messages seems reasonable
+    let mut buf = [0; 1024];
+    loop {
+        match socket.read(&mut buf).await {
+            Ok(0) => {
+                println!("client disconnected");
+                return Ok(());
+            },
+            Ok(n) => {
+                print!("Remote endpoint message: {}", String::from_utf8_lossy(&buf).trim());
+            },
+            Err(_) => {
+                panic!("data transfer phase error")
+            }
+
+        }
+    }
 }
