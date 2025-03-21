12110724183d956f8d5d0210d273caf6304fbab3
TmSalviano <tiagomsalviano@outlook.com>
2025-03-21 06:37:02 -0300

    Fail: attempt at creating chat server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-pub mod server;
 
-use core::panic;
-use std::io::Read;
-
-use server::Server;
+use std::sync::Arc;
 //matches many different Results for convenience
 use anyhow::Result;
-//logs at the info level
-use log::info;
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
+use tokio::sync::mpsc::{Receiver, Sender, channel};
 
 // Group Chat:
-//DataStructure to store the sockets
+//DataStructure to store the sockets -> Done
 //Task - listen to possible connections on a loop Client connections with the Server/Listener and add the
-//socket to the socket_store if active open connection as established
+//socket to the socket_store if active open connection as established.
+//  Change the active_open() function to do this and spawn a dedicated task for this.
 //Tasks to handle the sockets (TCP Streams)
 //in socket_handle() write the bytes from the data stream to all other clients
 
@@ -22,44 +19,46 @@ use tokio::net::{TcpListener, TcpStream};
 async fn main() -> Result<()> {
     let ip_addr = "127.0.0.1:8080";
     let server = passive_open(ip_addr).await?;
-    let socket = active_open(server).await;
-    handle_socket(socket).await?;
+    let (tx, rx) = channel(1024);
+    //(id, socket) collection
+    let clients: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(Vec::new()));
 
-    Ok(())
+    tokio::spawn(send_to_group(rx, clients.clone()));
+
+    loop {
+        let (mut socket, _) = server.accept().await?;
+        let tx = tx.clone();
+        let clients = clients.clone();
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+            clients.lock().await.push(socket);
+            if let Ok(n) = socket.read(&mut buf).await {
+                if n == 0 {
+                    println!("client disconnected");
+                };
+            }
+            tx.send(String::from_utf8_lossy(&buf).to_string())
+                .await
+                .unwrap();
+        });
+    }
 }
 
-async fn passive_open(ip_addr: &str) -> Result<TcpListener>{
+async fn passive_open(ip_addr: &str) -> Result<TcpListener> {
     let server = TcpListener::bind(ip_addr).await?;
     println!("Passive open was established");
     Ok(server)
 }
 
-async fn active_open(server: TcpListener) -> TcpStream {
-    let socket = match server.accept().await {
-        Ok((socket, _)) => socket,
-        Err(_) => panic!("Failed to establish an active open connection"),
-    };
-    socket
-}
-
-async fn handle_socket(mut socket: TcpStream) -> Result<()> {
-    //1kb capacity for messages seems reasonable
-    let mut buf = [0; 1024];
-    loop {
-        match socket.read(&mut buf).await {
-            Ok(0) => {
-                println!("client disconnected");
-                return Ok(());
-            }
-            Ok(_) => {
-                print!(
-                    "Remote endpoint message: {}",
-                    String::from_utf8_lossy(&buf).trim()
-                );
-            }
-            Err(_) => {
-                panic!("data transfer phase error")
-            }
+async fn send_to_group(
+    mut consumer: Receiver<String>,
+    ref mut clients: Arc<Mutex<Vec<TcpStream>>>,
+) {
+    while let Some(message) = consumer.recv().await {
+        let mut clients = clients.lock().await;
+        for client in &mut *clients {
+            client.write_all(message.as_bytes());
         }
     }
 }
